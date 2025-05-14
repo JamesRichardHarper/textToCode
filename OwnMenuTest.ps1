96f44dcfe1e43fa73694639f97e4e38a5a3f6c4e
James Harper <james-spare@live.co.uk>
2025-05-14 15:58:26 +0100

    Generic set properties is now sorted, pushing so I can test changing the new butto nfunction to work for any control function

--- OwnMenuTest.ps1
+++ OwnMenuTest.ps1
@@ -8,45 +8,47 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 #Default Variables
-$dContainerWidth = 300
-$dContainerHeight = 200
+$dContainerWidth = 450
+$dContainerHeight = 300
 
 [int32]$dTextboxWidth = ($dContainerWidth/100)*80
 [int32]$dTextboxHeight = ($dContainerHeight/100)*20
-
-$dButtonWidth = ($dContainerWidth/100)*20
-$dButtonHeight = ($dContainerHeight/100)*20
-
-$dText = ""
+[int32]$dButtonWidth = ($dContainerWidth/100)*20
+[int32]$dButtonHeight = ($dContainerHeight/100)*10
+[string]$dText = "notAssigned"
 
 #Functions
-# function createButton(
-#     [Int32]$buttonX,
-#     [Int32]$buttonY,
-#     [Int32]$sizeX,
-#     [Int32]$sizeY
-# ){
-
-# }
-function alignH([Int32]$pWidth){
-    #Given the width
-    #When the function triggers
-    #Then we return the location int needed to centre the object
-    return $dContainerWidth - $pWidth
+function Set-HoriontalAlign([Int32]$pWidth){
+    return $dContainerWidth/2 - $pWidth/2
 }
-function setGenericMeasurements(
-    $pObject,
-    [Int32]$pWidth, 
-    [Int32]$pHeight, 
-    [Int32]$pX, 
-    [Int32]$pY
+function Set-VerticalAlign([Int32]$pAlignHeight){
+    return $dContainerHeight/2 - $pAlignHeight/2
+}
+function Set-ControlProperties(
+    [Control]$pObject,
+    [Int32]$pWidth = 50,
+    [Int32]$pHeight = 50,
+    # [Int32]$pX = $dContainerWidth/2,
+    [Int32]$pX = {Set-HoriontalAlign -pWidth $pWidth},
+    [Int32]$pY = $dContainerHeight/2
 ){
-    $pObject.Location = [Point]::new($pX, $pY)
-    if (
-        ($pObject).GetType().Name -ne "TextBox"
-    ){
-        $pObject.Size = [Size]::new($pWidth, $pHeight)
-    }
+    $pObject.Location = [Point]::new($px, $pY)
+    $pObject.Size = [Size]::new($pWidth, $pHeight)
+}
+function New-Button(
+    [string]$buttonText = $dText,
+    [Int32]$buttonX,
+    [Int32]$buttonY,
+    [Int32]$buttonWidth = $dButtonWidth,
+    [Int32]$buttonHeight = $dButtonHeight,
+    [scriptblock]$buttonAction = {Write-Host "Not Assigned"}
+){
+    $functionButton = [Button]::new()
+    $functionButton.Text = $buttonText
+    Set-ControlProperties -pObject $functionButton -pWidth $buttonWidth -pHeight $buttonHeight -pX $buttonX -pY $buttonY
+    $functionButton.Add_Click($buttonAction)
+
+    return $functionButton
 }
 
 #Object Setup
@@ -59,34 +61,23 @@ $container.StartPosition = "CenterScreen"
 ##Textboxs
 ###Input
 $textPathInput = [TextBox]::new()
-try {
-    # setGenericMeasurements($textPathInput, $dTextboxWidth, $dTextboxHeight, alignH(dTextboxWidth), 50)
-    setGenericMeasurements
-        $textPathInput 
-        $dTextboxWidth
-        $dTextboxHeight 
-        50
-        50
-}
-catch {
-    Write-Output "Error:"
-    Write-Output $_
-}
-
 $textPathInput.Location = [Point]::new(50,50)
 
 ###Output
 $textPathOutput = [TextBox]::new()
-# setGenericMeasurements($textPathOutput, $dTextboxWidth, $dTextboxHeight, alignH(dTextboxWidth), 50)
 $textPathOutput.Location = [Point]::new(100,100)
 
 #Buttons
 $printButton = [Button]::new()
+# $printbutton2 = New-Button -buttonX 50 -buttonY 100
+$printbutton2 = New-Button -buttonX 100 -buttonY 100
+
 
 #Controls
-$container.Controls.Add($textPathInput)
-$container.Controls.Add($textPathOutput)
-$container.Controls.Add($printButton)
+$container.Controls.Add($printbutton2)
+# $container.Controls.Add($textPathInput)
+# $container.Controls.Add($textPathOutput)
+# $container.Controls.Add($printButton)
 
 # Show the form
 $container.Topmost = $true
