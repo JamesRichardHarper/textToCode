9239a93290d1dc56e36f168db21058d0c5db00d5
James Harper <james-spare@live.co.uk>
2025-05-15 01:26:25 +0100

    Functions and variables in place to track container size and place UI correctly now

--- OwnMenuTest.ps1
+++ OwnMenuTest.ps1
@@ -8,29 +8,37 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 #Default Variables
-$dContainerWidth = 450
-$dContainerHeight = 300
+$dFullWidth = 450
+$dFullHeight = $dFullWidth * (9/16)
+##Initial Container
+$container = [Form]::new()
+$container.Text = "App"
+$container.Size = [Size]::new($dFullWidth,$dFullHeight)
+$container.StartPosition = "CenterScreen"
 
-[int32]$dTextboxWidth = ($dContainerWidth/100)*80
-[int32]$dTextboxHeight = ($dContainerHeight/100)*20
-[int32]$dButtonWidth = ($dContainerWidth/100)*20
-[int32]$dButtonHeight = ($dContainerHeight/100)*10
+$containerWidth = $container.ClientSize.Width
+$containerHeight = $container.ClientSize.Height
+
+[int32]$dTextboxWidth = ($containerWidth/100)*80
+[int32]$dTextboxHeight = ($containerHeight/100)*20
+[int32]$dButtonWidth = ($containerWidth/100)*20
+[int32]$dButtonHeight = ($containerHeight/100)*10
 [string]$dText = "notAssigned"
 
 #Functions
 function Set-HoriontalAlign([Int32]$pWidth){
-    return $dContainerWidth/2 - $pWidth/2
+    return $containerWidth/2 - $pWidth/2
 }
 function Set-VerticalAlign([Int32]$pAlignHeight){
-    return $dContainerHeight/2 - $pAlignHeight/2
+    return $containerHeight/2 - $pAlignHeight/2
 }
 function Set-ControlProperties(
     [Control]$pObject,
     [Int32]$pWidth = 50,
     [Int32]$pHeight = 50,
-    # [Int32]$pX = $dContainerWidth/2,
+    # [Int32]$pX = $containerWidth/2,
     [Int32]$pX = {Set-HoriontalAlign -pWidth $pWidth},
-    [Int32]$pY = $dContainerHeight/2
+    [Int32]$pY = $containerHeight/2
 ){
     $pObject.Location = [Point]::new($px, $pY)
     $pObject.Size = [Size]::new($pWidth, $pHeight)
@@ -50,14 +58,37 @@ function New-Button(
 
     return $functionButton
 }
+# function New-Textbox(
+#     [Int32]$textboxX,
+#     [Int32]$textboxY,
+#     [Int32]$textboxWidth = $dButtonWidth,
+#     [Int32]$textboxHeight = $dButtonHeight,
+#     [scriptblock]$buttonAction = {Write-Host "Not Assigned"}
+# ){
+#     $functionButton = [Button]::new()
+#     $functionButton.Text = $buttonText
+#     Set-ControlProperties -pObject $functionButton -pWidth $buttonWidth -pHeight $buttonHeight -pX $buttonX -pY $buttonY
+#     $functionButton.Add_Click($buttonAction)
 
+#     return $functionButton
+# }
+# function New-Button(
+#     [string]$controlType = "Button",
+#     [string]$buttonText = $dText,
+#     [Int32]$buttonX,
+#     [Int32]$buttonY,
+#     [Int32]$buttonWidth = $dButtonWidth,
+#     [Int32]$buttonHeight = $dButtonHeight,
+#     [scriptblock]$buttonAction = {Write-Host "Not Assigned"}
+# ){
+#     $functionButton = [Button]::new()
+#     $functionButton.Text = $buttonText
+#     Set-ControlProperties -pObject $functionButton -pWidth $buttonWidth -pHeight $buttonHeight -pX $buttonX -pY $buttonY
+#     $functionButton.Add_Click($buttonAction)
+
+#     return $functionButton
+# }
 #Object Setup
-##Container
-$container = [Form]::new()
-$container.Text = "App"
-$container.Size = [Size]::new($dContainerWidth,$dContainerHeight)
-$container.StartPosition = "CenterScreen"
-
 ##Textboxs
 ###Input
 $textPathInput = [TextBox]::new()
@@ -68,16 +99,15 @@ $textPathOutput = [TextBox]::new()
 $textPathOutput.Location = [Point]::new(100,100)
 
 #Buttons
-$printButton = [Button]::new()
-# $printbutton2 = New-Button -buttonX 50 -buttonY 100
-$printbutton2 = New-Button -buttonX 100 -buttonY 100
+$exitButton = New-Button -buttonText "exit" -buttonX ($containerWidth-($dButtonWidth*1.75)) -buttonY ($containerHeight-($dButtonHeight*2))
+$printButton = New-Button -buttonText "testConsole" -buttonX ($dButtonWidth*0.75) -buttonY ($containerHeight-($dButtonHeight*2))
 
 
 #Controls
-$container.Controls.Add($printbutton2)
-# $container.Controls.Add($textPathInput)
-# $container.Controls.Add($textPathOutput)
-# $container.Controls.Add($printButton)
+$container.Controls.Add($exitButton)
+$container.Controls.Add($printButton)
+$container.Controls.Add($textPathInput)
+$container.Controls.Add($textPathOutput)
 
 # Show the form
 $container.Topmost = $true
